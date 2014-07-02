1a1b2ba832289f784a0a23da5f09646d2e0d50d2
Brandon Hansen <bh@jesusculture.com>
2014-07-01 21:50:07 -0700

    gfycat embed

--- app/models/link.rb
+++ app/models/link.rb
@@ -21,6 +21,14 @@ class Link
       false
   end
 
+  def gfycat?
+    @link.url =~ /gfycat\.com/i
+  end
+
+  def gfycat_embed
+    "<iframe src='http://gfycat.com/ifr/#{@link.url.split("/").last}' frameborder='0' scrolling='no' width='100%' height='314' ></iframe>"
+  end
+
   def picture?
     @link.url =~ /\.png|\.jpg|\.jpeg|\.gif/i
   end
