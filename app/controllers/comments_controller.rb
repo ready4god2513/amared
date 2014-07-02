ace2a944f88f30351eb35d24399ef0e4c41d7372
Brandon Hansen <bh@jesusculture.com>
2014-07-02 15:30:36 -0700

    Fixing comments link

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def index
     RedditKit.sign_in ENV["REDDIT_USERNAME"], ENV["REDDIT_PASSWORD"]
     @sub = Subreddit.new(params[:subreddit] || "pics")
-    @item = Link.new(RedditKit.link "t3_#{params[:id]}")
+    @item = Link.new(RedditKit.link "t3_#{params[:id].gsub(/t3_/, "")}")
     @comments = RedditKit.comments @item.link, sort: params[:sort] || "confidence"
   end
 
