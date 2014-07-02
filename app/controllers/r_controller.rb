9157d84a113ee0f7e4c7ad29f9e5517d6c973c9b
Brandon Hansen <bh@jesusculture.com>
2014-07-01 20:20:13 -0700

    Pagination for new

--- app/controllers/r_controller.rb
+++ app/controllers/r_controller.rb
@@ -3,8 +3,15 @@ class RController < ApplicationController
   def show
     if params[:subreddit] == "all"
       RedditKit.sign_in ENV["REDDIT_USERNAME"], ENV["REDDIT_PASSWORD"]
-      @sub = OpenStruct.new(name: "all", attributes: { subscribers: Random.new.rand(10000000) }, options: params)
       @links = RedditKit.links("all", params.dup)
+      
+      @sub = OpenStruct.new(name: "all", 
+        attributes: { 
+          subscribers: Random.new.rand(10000000)
+        }, 
+        options: params.merge({ after: @links.after })
+      )
+
       @output = @links.map { |l| Link.new(l) }
     else
       @sub = Subreddit.new(params[:subreddit] || "pics")
