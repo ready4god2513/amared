ad077ff8cda942ed525ef5010d640f4a951c26ef
Brandon Hansen <bh@jesusculture.com>
2014-07-02 22:36:22 -0700

    Much better searching

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,9 +1,9 @@
-var app = angular.module('amared', ['ngResource']);
+var app = angular.module('amared', ['ngResource', 'ui.utils']);
 
 app.factory('Amared', ['$http', function($http){
   return {
     recent: function(subreddit, callback){
-      $http.get('/r/' + subreddit + '/comments.json?limit=1000').success(function(response){
+      $http.get('/r/' + subreddit + '/comments.json?limit=100').success(function(response){
         callback(response);
       });
     }
@@ -21,10 +21,17 @@ app.controller('CommentController', ['$scope', '$timeout', 'Amared', function($s
 
   $scope.comments = [];
   $scope.subreddit = 'all';
+  $scope.Search = '';
 
   $scope.getRecentComments = function(){
     Amared.recent($scope.subreddit, function(res){
-      $scope.comments = res;
+      if($scope.Search.length){
+        $scope.comments = $scope.comments.concat(res);
+      }
+      else
+      {
+        $scope.comments = res;
+      }
       $timeout(function(){
         $scope.getRecentComments();
       }, 1000);
