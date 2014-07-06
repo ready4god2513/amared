801b74e3b968a3dc87fd7f429d9ba1a815a5064f
Brandon Hansen <bh@jesusculture.com>
2014-07-05 22:33:00 -0700

    Adding streaming comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,9 +1,9 @@
-var app = angular.module('amared', ['ngResource', 'ui.utils']);
+var app = angular.module('amared', ['ngResource', 'ui.utils', 'ngAnimate']);
 
 app.factory('Amared', ['$http', function($http){
   return {
     recent: function(subreddit, callback){
-      $http.get('/r/' + subreddit + '/comments.json?limit=100').success(function(response){
+      $http.get('/r/' + subreddit + '/comments.json?limit=20').success(function(response){
         callback(response);
       });
     }
@@ -20,23 +20,37 @@ app.filter('unsafe', function($sce) {
 app.controller('CommentController', ['$scope', '$timeout', 'Amared', function($scope, $timeout, Amared){
 
   $scope.comments = [];
+  $scope.allComments = [];
   $scope.subreddit = 'all';
   $scope.Search = '';
+  $scope.maxLength = 100;
 
   $scope.getRecentComments = function(){
     Amared.recent($scope.subreddit, function(res){
-      if($scope.Search.length){
-        $scope.comments = $scope.comments.concat(res);
-      }
-      else
-      {
-        $scope.comments = res;
-      }
+      $scope.allComments = $scope.allComments.concat(res);
       $timeout(function(){
         $scope.getRecentComments();
-      }, 1000);
+      }, 3000);
 
     });
   };
+
+  $scope.addNewComment = function(){
+    var allCommentsLength = $scope.allComments.length;
+
+    if(allCommentsLength){
+      $scope.comments.unshift($scope.allComments.shift());
+
+      if($scope.comments.length > $scope.maxLength){
+        $scope.comments.pop();
+      }
+    }
+
+    $timeout(function(){
+      $scope.addNewComment();
+    }, Math.random() * 1500);
+  }
+
+  $scope.addNewComment();
 
 }]);
